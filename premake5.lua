e4e8924033f94af2fba62cc51a4fbea28f7f8218
aktham12 <aktham.alhmedat55@gmail.com>
2021-12-23 17:14:12 +0200

    Added Premake

---
+++ premake5.lua
@@ -0,0 +1,110 @@
+workspace "Kernstuck"
+    architecture "x64"
+    configurations
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+    outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Kernstuck"
+    location "Kernstuck"
+    kind "SharedLib"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "%{prj.name}/vendor/spdlog/include"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines
+        {
+            "KS_PLATFORM_WINDOWS",
+            "KS_BUILD_DLL",
+            
+        }
+
+        postbuildcommands
+        {
+            ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/FlappyBird")
+        }
+
+    filter "configurations:Debug"
+        defines "KS_DEBUG"
+        symbols "On"
+    
+    filter "configurations:Release"
+        defines "KS_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "KS_DIST"
+        optimize "On"
+    
+project "FlappyBird"
+        location "FlappyBird"
+        kind "ConsoleApp"
+        language "C++"
+
+        targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+        objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+        files
+        {
+            "%{prj.name}/src/**.h",
+            "%{prj.name}/src/**.cpp"
+        }
+
+        includedirs
+        {
+            "Kernstuck/vendor/spdlog/include",
+            "Kernstuck/src"
+        }
+
+        links
+        {
+            "Kernstuck"
+        }
+
+        filter "system:windows"
+            cppdialect "C++17"
+            staticruntime "On"
+            systemversion "latest"
+
+            defines
+            {
+                "KS_PLATFORM_WINDOWS"
+            }
+
+        filter "configurations:Debug"
+            defines "KS_DEBUG"
+            symbols "On"
+
+        filter "configurations:Release"
+            defines "KS_RELEASE"
+            optimize "On"
+
+        filter "configurations:Dist"
+            defines "KS_DIST"
+            optimize "On"
+
+            
+
+
+   
